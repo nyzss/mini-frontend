220a338f190a9ca53cb18299772b01a13e04469c
Okan Koca <oknakoca@gmail.com>
2024-10-13 18:09:26 +0200

    implemented player1 and player2, movement and also modularized the code more

--- srcs/ts/game.ts
+++ srcs/ts/game.ts
@@ -2,6 +2,106 @@ import { tag } from "./handler.js";
 import { Routes } from "./types";
 
 let animFrame: number;
+const ball = {
+	x: 75,
+	y: 50,
+	vx: 5,
+	vy: 2,
+	maxvX: 10,
+	maxvY: 4,
+	radius: 17.5,
+	color: "red",
+	move(canvas: HTMLCanvasElement) {
+		this.x += this.vx;
+		this.y += this.vy;
+
+		if (this.x + this.radius >= canvas.width || this.x - this.radius <= 0) {
+			if (this.vx > 0 && this.vx < this.maxvX) this.vx += 1;
+			else if (this.vx < 0 && this.vx > -this.maxvX) this.vx -= 1;
+			this.vx *= -1;
+		}
+		if (
+			this.y + this.radius >= canvas.height ||
+			this.y - this.radius <= 0
+		) {
+			if (this.vy > 0 && this.vy < this.maxvY) this.vy += 1;
+			else if (this.vy < 0 && this.vy > -this.maxvY) this.vy -= 1;
+			this.vy *= -1;
+		}
+		// console.log("Ball X: ", this.x, ", velocity X: ", this.vx);
+		// console.log("Ball Y: ", this.y, ", velocity Y: ", this.vy);
+		return this;
+	},
+	draw(ctx: CanvasRenderingContext2D) {
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+		ctx.closePath();
+		ctx.fillStyle = this.color;
+		ctx.fill();
+		return this;
+	},
+};
+
+const paddleLeft = {
+	x: 0,
+	y: 50,
+	vy: 10,
+	width: 50,
+	height: 100,
+	color: "blue",
+	keys: {
+		up: false,
+		down: false,
+		upKey: "w",
+		downKey: "s",
+	},
+	draw(ctx: CanvasRenderingContext2D) {
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+		return this;
+	},
+	move(ctx: CanvasRenderingContext2D) {
+		if (this.keys.up) this.y -= this.vy;
+		if (this.keys.down) this.y += this.vy;
+	},
+	keyHandler(event: KeyboardEvent, value: boolean) {
+		if (event.key == this.keys.upKey) this.keys.up = value;
+		if (event.key == this.keys.downKey) this.keys.down = value;
+	},
+};
+
+const paddleRight = {
+	x: 0,
+	y: 50,
+	vy: 10,
+	width: 50,
+	height: 100,
+	color: "green",
+	keys: {
+		up: false,
+		down: false,
+		upKey: "ArrowUp",
+		downKey: "ArrowDown",
+	},
+	draw(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+		ctx.fillStyle = this.color;
+		ctx.fillRect(
+			canvas.width - this.width,
+			this.y,
+			this.width,
+			this.height
+		);
+		return this;
+	},
+	move(ctx: CanvasRenderingContext2D) {
+		if (this.keys.up) this.y -= this.vy;
+		if (this.keys.down) this.y += this.vy;
+	},
+	keyHandler(event: KeyboardEvent, value: boolean) {
+		if (event.key == this.keys.upKey) this.keys.up = value;
+		if (event.key == this.keys.downKey) this.keys.down = value;
+	},
+};
 
 export const gameHandler = (route: Routes) => {
 	console.log("current route: ", route.description);
@@ -11,55 +111,20 @@ export const gameHandler = (route: Routes) => {
 	) as HTMLCanvasElement;
 	const ctx = gameBoard.getContext("2d");
 
-	console.log(gameBoard);
-	console.log(ctx);
+	// console.log(gameBoard);
+	// console.log(ctx);
 
-	const ball = {
-		x: 75,
-		y: 50,
-		vx: 5,
-		vy: 2,
-		maxvX: 10,
-		maxvY: 4,
-		radius: 17.5,
-		color: "red",
-		move() {
-			this.x += this.vx;
-			this.y += this.vy;
-
-			if (
-				this.x + this.radius >= gameBoard.width ||
-				this.x - this.radius <= 0
-			) {
-				if (this.vx > 0 && this.vx < this.maxvX) this.vx += 1;
-				else if (this.vx < 0 && this.vx > -this.maxvX) this.vx -= 1;
-				this.vx *= -1;
-			}
-			if (
-				this.y + this.radius >= gameBoard.height ||
-				this.y - this.radius <= 0
-			) {
-				if (this.vy > 0 && this.vy < this.maxvY) this.vy += 1;
-				else if (this.vy < 0 && this.vy > -this.maxvY) this.vy -= 1;
-				this.vy *= -1;
-			}
-			console.log("Ball X: ", this.x, ", velocity X: ", this.vx);
-			console.log("Ball Y: ", this.y, ", velocity Y: ", this.vy);
-			return this;
-		},
-		draw() {
-			ctx.beginPath();
-			ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-			ctx.closePath();
-			ctx.fillStyle = this.color;
-			ctx.fill();
-			return this;
-		},
+	const clear = () => {
+		// ctx.clearRect(0, 0, gameBoard.width, gameBoard.height);
+		ctx.fillStyle = "rgb(255 255 255 / 30%)";
+		ctx.fillRect(0, 0, gameBoard.width, gameBoard.height);
 	};
 
 	const draw = () => {
-		ctx.clearRect(0, 0, gameBoard.width, gameBoard.height);
-		ball.draw().move();
+		clear();
+		ball.draw(ctx).move(gameBoard);
+		paddleLeft.draw(ctx).move(gameBoard);
+		paddleRight.draw(gameBoard, ctx).move(gameBoard);
 		animFrame = window.requestAnimationFrame(draw);
 	};
 
@@ -71,7 +136,18 @@ export const gameHandler = (route: Routes) => {
 		window.cancelAnimationFrame(animFrame);
 	});
 
-	ball.draw();
+	document.addEventListener("keydown", (e) => {
+		paddleLeft.keyHandler(e, true);
+		paddleRight.keyHandler(e, true);
+	});
+
+	document.addEventListener("keyup", (e) => {
+		paddleLeft.keyHandler(e, false);
+		paddleRight.keyHandler(e, false);
+	});
+
+	ball.draw(ctx);
+	paddleLeft.draw(ctx);
 
 	const elements = tag("div", "hello world!");
 	entry.appendChild(elements);
